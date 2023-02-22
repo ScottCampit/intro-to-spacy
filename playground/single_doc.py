43804db59d7c4bd5c7b94502bbd916995427577c
scampit <scottcampit@gmail.com>
2023-02-22 10:56:52 -0800

    Added single doc example

---
+++ playground/single_doc.py
@@ -0,0 +1,11 @@
+# Single spaCy document example
+import spacy
+
+# Load spacy object that contains tokenizer, tagger, parser, and named entity recognition modules.
+nlp = spacy.load('en_core_web_sm')
+print(nlp.pipe_names)
+
+text = 'This is a sentence about nothing.'
+
+# Convert the string into a Doc object
+doc = nlp(text)
